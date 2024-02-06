dada969077bf2fb2668f3969cc57347bd02e790d
신한솔 <shinhansol@OneSol-MacBook.local>
2024-02-06 20:45:14 +0900

    first commit

---
+++ src/main/java/com/example/supercoding2stsohee/Supercoding2stSoheeApplication.java
@@ -0,0 +1,13 @@
+package com.example.supercoding2stsohee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Supercoding2stSoheeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Supercoding2stSoheeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/supercoding2stsohee/Supercoding2stSoheeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supercoding2stsohee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Supercoding2stSoheeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
