d0f9da8d6baf926ab8d9515432398ac4c5fee7ed
soheeparklee <soheeparklee@gmail.com>
2024-02-09 16:41:27 +0900

    feat: package name update

--- src/main/java/com/example/supercoding2stsohee/repository/users/UserJpa.java
+++
@@ -1,8 +0,0 @@
-package com.example.supercoding2stsohee.repository.users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserJpa extends JpaRepository<User, Integer> {
-}
